5f002f896e2b114127cb496e704e15a239cb7d96
nogayegueye <nogayegye@gmail.com>
2023-06-08 02:03:28 +0200

    Fin du projet

--- kasa/src/components/Image_Banner.js
+++ kasa/src/components/Image_Banner.js
@@ -2,42 +2,51 @@ import React, { useState } from 'react';
 import './Image_banner.scss';
 
 function Image_Banner(props) {
-      const pitcures = props.pictures
+      const pitcures = props.pictures;
 
-      const[currentPicture, setcurrentPicture] = useState(0);
+      const [currentPicture, setcurrentPicture] = useState(0);
 
-      const getClassName = (i) =>{
-            if (i === currentPicture) return "show";
-            return"";
-      }
-      
-      const moveToNext =() =>{
+      const getClassName = (i) => {
+            if (i === currentPicture) return 'show';
+            return '';
+      };
+
+      const moveToNext = () => {
             setcurrentPicture((currentPicture + 1) % pitcures.length);
       };
-      
-      const moveToPrevious =() =>{
+
+      const moveToPrevious = () => {
             const newCurrentPicture = currentPicture - 1;
-            if (newCurrentPicture < 0){
+            if (newCurrentPicture < 0) {
                   setcurrentPicture(pitcures.length - 1);
                   return;
             }
             setcurrentPicture(currentPicture - 1);
       };
 
-
       return (
             <div className="banner_image">
-                  <div className='image_container'>
-                  {pitcures.map((pic, i) => <img key={pic} src={pic} alt='' className={getClassName(i)}></img>)}
+                  <div className="image_container">
+                        {pitcures.map((pic, i) => (
+                              <img
+                                    key={pic}
+                                    src={pic}
+                                    alt=""
+                                    className={getClassName(i)}
+                              ></img>
+                        ))}
                   </div>
-                  <button className='btn_previous' onClick={moveToPrevious}>
-                        <i className='fa-solid fa-chevron-left'></i>
+                  <button className="btn_previous" onClick={moveToPrevious}>
+                        <i className="fa-solid fa-chevron-left"></i>
                   </button>
-                  <button className='btn_next' onClick={moveToNext}>
-                  <i className='fa-solid fa-chevron-right'></i>
+                   <span className='slide_cunter'>
+                        {currentPicture + 1} / {pitcures.length}
+                  </span> 
+                  <button className="btn_next" onClick={moveToNext}>
+                        <i className="fa-solid fa-chevron-right"></i>
                   </button>
             </div>
       );
 }
 
-export default Image_Banner ;
+export default Image_Banner;
